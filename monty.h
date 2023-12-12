759a7f9032fe3f8eee35be4a95f8e1c5d9e7ffd7
Casey Paul <7227@holbertonstudents.com>
2023-12-12 12:49:34 -0800

    push and pall builtin functions

---
+++ monty.h
@@ -0,0 +1,63 @@
+#ifndef __MONTY_H__
+#define __MONTY_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+
+#define QUEUE 1
+#define STACK 0
+#define DELIM " \n\t\a\b"
+
+/**
+ * globales_s - struct with variable globals
+ * @token2: integer
+ * @fd: points to the previous element of the stack (or queue)
+ * @line_buf: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct globales_s
+{
+	char *token2;
+	FILE *fd;
+	char *line_buf;
+
+} globales_t;
+
+extern globales_t globalvar;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* main.c */
+int main(int argc, char **argv);
+
+/* node functions */
+stack_t *create_node_stackfirst(stack_t **stack, int n);
+stack_t *create_node_stackend(stack_t **stack, int n);
+void free_node_stack(stack_t **stack);
+
+/* get_builtin */
+int get_builtin(char *token, stack_t **stack, unsigned int line_number);
+/* builtins functions */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
